e9fa06f57939a266c71442228a27b2f16d4eb29e
choeyoha <yoha6865@naver.com>
2023-02-15 22:00:09 +0900

    commit test

---
+++ variable_01/convention.js
@@ -0,0 +1,14 @@
+var resultVal;
+// 카멜 케이스 camel Case
+
+var ResultVal;
+// 파스칼 케이스 Pascal Case
+
+var result_val;
+// 스네이크 케이스 snake_case
+
+var iVal;
+// 헝가리안 케이스
+// type(값의 타입) + identifier(식별자)
+
+//asdf
---
+++ variable_01/rule.js
@@ -0,0 +1,7 @@
+var first_result, $res, _val;
+// 가능
+
+var 1first, if, while;
+// 숫자로 시작 or 예약어 사용으로 불가능
+
+//asdf
