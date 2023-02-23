2b2ff58fc0bfe2454ce65cfeda7641e81b89ba38
choeyoha <yoha6865@naver.com>
2023-02-23 21:42:05 +0900

    220223 03_data_type practice code

---
+++ data_type_03/symbol.js
@@ -0,0 +1,4 @@
+// 심벌 타입의 값 생성
+var key = Symbol('key'); // Symbol 함수를 호출하여 생성
+
+console.log(typeof key);
---
+++ data_type_03/template_literal.js
@@ -0,0 +1,5 @@
+var html_code = `<div>
+   <span> multi_line string </span>
+</div>`;
+
+console.log(html_code);
